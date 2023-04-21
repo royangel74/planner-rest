e561b5e5f8895d56f75e6444702c63da3107f6e6
RiccardoBagaglini <riccardo.bagaglini@tech.iagica.it>
2023-04-21 08:23:26 +0200

    Sviluppo GlobalExceptionHandler

---
+++ src/main/java/com/iagica/training/plannerrest/configuration/GlobalExceptionHandler.java
@@ -0,0 +1,132 @@
+package com.iagica.training.plannerrest.configuration;
+
+import com.iagica.training.plannerrest.domain.exception.NotFoundException;
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ValidationException;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.Optional.ofNullable;
+
+public class GlobalExceptionHandler {
+    private final Logger logger = LogManager.getLogger();
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ApiCallError<String>> handleNotFoundException(HttpServletRequest request, NotFoundException ex) {
+        logger.error("NotFoundException {}\n", request.getRequestURI(), ex);
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ApiCallError<>("Not found exception", List.of(ex.getMessage())));
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<ApiCallError<String>> handleValidationException(HttpServletRequest request, ValidationException ex) {
+        logger.error("ValidationException {}\n", request.getRequestURI(), ex);
+
+        return ResponseEntity.badRequest()
+                .body(new ApiCallError<>("Validation exception", List.of(ex.getMessage())));
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<ApiCallError<String>> handleMissingServletRequestParameterException(HttpServletRequest request, MissingServletRequestParameterException ex) {
+        logger.error("handleMissingServletRequestParameterException {}\n", request.getRequestURI(), ex);
+
+        return ResponseEntity.badRequest()
+                .body(new ApiCallError<>("Missing request parameter", List.of(ex.getMessage())));
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ApiCallError<Map<String, String>>>
+    handleMethodArgumentTypeMismatchException(HttpServletRequest request, MethodArgumentTypeMismatchException ex) {
+        logger.error("handleMethodArgumentTypeMismatchException {}\n", request.getRequestURI(), ex);
+
+        var details = new HashMap<String, String>();
+        details.put("paramName", ex.getName());
+        details.put("paramValue", ofNullable(ex.getValue()).map(Object::toString).orElse(""));
+        details.put("errorMessage", ex.getMessage());
+
+        return ResponseEntity.badRequest()
+                .body(new ApiCallError<>("Method argument type mismatch", List.of(details)));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiCallError<Map<String, String>>> handleMethodArgumentNotValidException(HttpServletRequest request, MethodArgumentNotValidException ex) {
+        logger.error("handleMethodArgumentNotValidException {}\n", request.getRequestURI(), ex);
+
+        var details = new ArrayList<Map<String, String>>();
+        ex.getBindingResult()
+                .getFieldErrors()
+                .forEach(
+                        fieldError -> {
+                            Map<String, String> detail = new HashMap<>();
+                            detail.put("objectName", fieldError.getObjectName());
+                            detail.put("field", fieldError.getField());
+                            detail.put("rejectedValue", "" + fieldError.getRejectedValue());
+                            detail.put("errorMessage", fieldError.getDefaultMessage());
+                            details.add(detail);
+                        });
+
+        return ResponseEntity.badRequest()
+                .body(new ApiCallError<>("Method argument validation failed", details));
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ApiCallError<String>> handleAccessDeniedException(HttpServletRequest request, AccessDeniedException ex) {
+        logger.error("handleAccessDeniedException {}\n", request.getRequestURI(), ex);
+
+        return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                .body(new ApiCallError<>("Access denied!", List.of(ex.getMessage())));
+    }
+
+    @ExceptionHandler(ExpiredJwtException.class)
+    public ResponseEntity<ApiCallError<String>> handleExpiredJwtException(HttpServletRequest request,  ExpiredJwtException ex) {
+        logger.error("ExpiredJwtException {}\n", request.getRequestURI(), ex);
+
+        return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                .body(new ApiCallError<>("JWT token expired!", List.of(ex.getMessage())));
+    }
+
+    @ExceptionHandler(IOException.class)
+    public ResponseEntity<ApiCallError<String>> handleIOException(HttpServletRequest request,  ExpiredJwtException ex) {
+        logger.error("IOException {}\n", request.getRequestURI(), ex);
+
+        return this.handleInternalServerError(request, ex);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiCallError<String>> handleInternalServerError(
+            HttpServletRequest request, Exception ex) {
+        logger.error("handleInternalServerError {}\n", request.getRequestURI(), ex);
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ApiCallError<>("Internal server error", List.of(ex.getMessage())));
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ApiCallError<T> {
+        private String message;
+        private List<T> details;
+    }
+
+}
