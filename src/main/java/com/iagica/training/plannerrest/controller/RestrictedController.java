2fd1079b94f0fe3226c82862216218dc18e96ea0
Roberto Angelucci <roberto.angelucci@gmail.com>
2023-06-09 15:54:32 +0200

    Fix: add check role information from token for strong security

--- src/main/java/com/iagica/training/plannerrest/controller/RestrictedController.java
+++ src/main/java/com/iagica/training/plannerrest/controller/RestrictedController.java
@@ -2,10 +2,14 @@ package com.iagica.training.plannerrest.controller;
 
 import com.iagica.training.plannerrest.domain.model.helper.Access;
 import com.iagica.training.plannerrest.domain.model.helper.Function;
+import com.iagica.training.plannerrest.domain.model.helper.User;
 import com.iagica.training.plannerrest.repository.helper.AccessRepository;
 import com.iagica.training.plannerrest.repository.helper.FunctionRepository;
+import com.iagica.training.plannerrest.repository.helper.UserRepository;
 import com.iagica.training.plannerrest.utils.ApplicationUtility;
+import com.iagica.training.plannerrest.utils.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 
@@ -17,6 +21,9 @@ public abstract class RestrictedController {
     private FunctionRepository functionRepository;
 
     @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
     private AccessRepository accessRepository;
 
     public abstract String getPreauthorizeIdFunction();
@@ -24,6 +31,10 @@ public abstract class RestrictedController {
     public boolean hasPrivilege(Authentication authentication, String idAccess) {
         Optional<? extends GrantedAuthority> authority = authentication.getAuthorities().stream().findFirst();
         if (authority.isPresent()) {
+            Optional<User> user = userRepository.searchUserWithRole(authentication.getName());
+            if (!user.isEmpty() && !user.get().getRole().toString().equals(authority.get().getAuthority())) {
+                throw new AccessDeniedException(Constants.MSG_ACCESS_DENIED_ERROR_01);
+            }
             Integer weightRole = ApplicationUtility.getWeightFromRoleString(authority.get().getAuthority());
             Optional<Function> function = functionRepository.findByIdfunction(this.getPreauthorizeIdFunction());
             Optional<Access> access = accessRepository.findByIdaccess(idAccess);
--- src/main/java/com/iagica/training/plannerrest/utils/Constants.java
+++ src/main/java/com/iagica/training/plannerrest/utils/Constants.java
@@ -7,4 +7,6 @@ public class Constants {
     public static final String EVENT_CONTROLLER_FUNCTION = "EVENTS";
 
     public static final String CONFIG_ROLE_KEY_NAME = "DEFAULT_ROLE";
+
+    public static final String MSG_ACCESS_DENIED_ERROR_01 = "Security token violation: attribute received not valid";
 }
